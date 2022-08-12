e1e3af28513528707a36190d82f529e3a791ca92
Christian Nielsen <chrnielsen2003@gmail.com>
2022-08-12 10:37:58 +0200

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   /* const blueOnPage = "#3A798B"
   const greyOnPage = "#333333"
+  const lightGrey = #E6E6E6
    */
 
   return (
@@ -31,13 +32,13 @@ function App() {
                   <h3 style={{marginLeft:"1em"}}>Kompetancer</h3>
                 </div>
                 <div style={{flex: "1", backgroundColor:"#333"}}>
-                  <h3 style={{paddingLeft: "2.2em"}}>Kontakt</h3>
+                  <h3 style={{paddingLeft: "1.5em"}}>Kontakt</h3>
                 </div>
               </div>
 
-              <div>
-                <div className='info'>
-                  <p>Jeg er handlings orienteret, rationel med et rolig sind, jeg
+              <div style={{ display:"flex", fontSize: "0.8em" }}>
+                <div className='info' style={{flex: "1"}}>
+                  <p style={{marginLeft:"2.5em"}}>Jeg er handlings orienteret, rationel med et rolig sind, jeg
 kan have flere bolde i luften, ser tit ting fra forskellige
 vinkler. Jeg har en motivation i at lære nye ting inden for
 IT, lytter gerne til andres meninger i forhold til
@@ -47,10 +48,10 @@ personligt. Jeg ser fordelen i at acceptere andres ideer og
 erfaring, da jeg med min unge alder ser muligheder i at
 modtage input.</p>
                 </div>
-                <div className='contactInfo'>
-                  <div><a href='tel:25771403'>25 77 14 03</a></div>
-                  <div><p>Munkholmvej 389, 4060 Kirke Såby</p></div>
-                  <div><a href='mailto:chrnielsen2003@gmail.com'>chrnielsen2003@gmail.com</a></div>
+                <div style={{flex: 1, backgroundColor: "#E6E6E6", paddingTop:".5em"}} className='contactInfo'>
+                  <div style={{ marginLeft:"2em" }}> <i class="fa-solid fa-phone"></i> <a href='tel:25771403'>25 77 14 03</a></div>
+                  <div style={{ marginLeft:"2em", display:"flex", alignItems:"center", marginTop:".4em" }}><i class="fa-solid fa-house"></i><p style={{marginLeft:"3px"}}>Munkholmvej 389, 4060 Kirke Såby</p></div>
+                  <div style={{ marginLeft:"2em" }}><i class="fa-solid fa-envelope"></i> <a href='mailto:chrnielsen2003@gmail.com'>chrnielsen2003@gmail.com</a></div>
                 </div>
               </div>
 
